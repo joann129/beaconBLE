d07e149fb8f717f668dc68f7e472afa1780242be
江怡均 <a0905187634@gmail.com>
2021-08-30 20:02:00 +0800

    first commit

---
+++ app/src/main/java/com/example/ble/MainActivity.java
@@ -0,0 +1,549 @@
+/*
+Android Example to connect to and communicate with Bluetooth
+In this exercise, the target is a Arduino Due + HC-06 (Bluetooth Module)
+
+Ref:
+- Make BlueTooth connection between Android devices
+http://android-er.blogspot.com/2014/12/make-bluetooth-connection-between.html
+- Bluetooth communication between Android devices
+http://android-er.blogspot.com/2014/12/bluetooth-communication-between-android.html
+ */
+package com.example.ble;
+
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ble.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements BeaconConsumer {
+
+    //beacon
+    private static final String TAG = "MainActivity";
+    private TextView tvLocationMsg;
+    private static final long DEFAULT_FOREGROUND_SCAN_PERIOD = 1000L;
+    private static final long DEFAULT_FOREGROUND_BETWEEN_SCAN_PERIOD = 1000L;
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+    private BeaconManager beaconManager;
+    /** 重新調整格式*/
+    public static final String IBEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24";
+    /** 設置興趣UUID*/
+    public static final String FILTER_UUID = "D5A7867E-A332-5E85-3B40-E8425F94D674";
+    //D5A7867E-A332-5E85-3B40-E8425F94D674
+//    public BeaconLocationData beaconLocationData;
+
+    //BLE
+    private static final int REQUEST_ENABLE_BT = 1;
+
+    BluetoothAdapter bluetoothAdapter;
+
+    ArrayList<BluetoothDevice> pairedDeviceArrayList;
+
+    TextView textInfo, textStatus;
+    ListView listViewPairedDevice;
+    LinearLayout inputPane;
+    EditText inputField;
+    Button btnSend, btnClear;
+
+    ArrayAdapter<BluetoothDevice> pairedDeviceAdapter;
+    private UUID myUUID;
+    private final String UUID_STRING_WELL_KNOWN_SPP =
+            "00001101-0000-1000-8000-00805F9B34FB";
+
+    ThreadConnectBTdevice myThreadConnectBTdevice;
+    ThreadConnected myThreadConnected;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //beacon
+        initView();
+        requestLocationPermissions();
+        initBeacon();
+        initData();
+
+        textInfo = (TextView)findViewById(R.id.info);
+        textStatus = (TextView)findViewById(R.id.status);
+//        textByteCnt = (TextView)findViewById(R.id.textbyteCnt);
+        listViewPairedDevice = (ListView)findViewById(R.id.pairedlist);
+
+        inputPane = (LinearLayout)findViewById(R.id.inputpane);
+        inputField = (EditText)findViewById(R.id.input);
+        btnSend = (Button)findViewById(R.id.send);
+        btnSend.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                if(myThreadConnected!=null){
+                    byte[] bytesToSend = inputField.getText().toString().getBytes();
+                    myThreadConnected.write(bytesToSend);
+                    byte[] NewLine = "\n".getBytes();
+                    myThreadConnected.write(NewLine);
+                }
+            }});
+
+        btnClear = (Button)findViewById(R.id.clear);
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textStatus.setText("");
+//                textByteCnt.setText("");
+            }
+        });
+
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)){
+            Toast.makeText(this,
+                    "FEATURE_BLUETOOTH NOT support",
+                    Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        //using the well-known SPP UUID
+        myUUID = UUID.fromString(UUID_STRING_WELL_KNOWN_SPP);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this,
+                    "Bluetooth is not supported on this hardware platform",
+                    Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        String stInfo = bluetoothAdapter.getName() + "\n" +
+                bluetoothAdapter.getAddress();
+        textInfo.setText(stInfo);
+    }
+
+    //beacon
+    private void requestLocationPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            // Android M Permission check
+
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+                builder.setTitle("This app needs location access");
+                builder.setMessage("Please grant location access so this app can detect beacons.");
+                builder.setPositiveButton(android.R.string.ok, null);
+
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+
+                    @Override
+
+                    public void onDismiss(DialogInterface dialog) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+
+                    }
+
+
+                });
+
+                builder.show();
+
+            }
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "coarse location permission granted");
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionality limited");
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                        }
+
+                    });
+                    builder.show();
+                }
+                return;
+            }
+        }
+    }
+
+    private void initData() {
+//        beaconLocationData = new BeaconLocationData();
+    }
+
+    private void initBeacon() {
+//        獲取beaconManager實例對象
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+//        設置搜索的時間間隔和週期
+        beaconManager.setForegroundBetweenScanPeriod(DEFAULT_FOREGROUND_BETWEEN_SCAN_PERIOD);
+        beaconManager.setForegroundScanPeriod(DEFAULT_FOREGROUND_SCAN_PERIOD);
+//        設置beacon數據包格式
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(IBEACON_FORMAT));
+//        將activity與庫中的BeaconService綁定到一起,服務準備完成後就會自動回調下面的onBeaconServiceConnect方法
+        beaconManager.bind(this);
+
+    }
+
+    private void initView() {
+        tvLocationMsg = (TextView) findViewById(R.id.tv_location_msg);
+    }
+
+    /**
+     * BeaconConsumer類的回調方法，當BeaconService準備完畢時調用該方法
+     */
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.addRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
+                Log.i(TAG, "didRangeBeaconsInRegion: 調用了這個方法:"+collection.size());
+//                updateTextViewMsg("進入didRangeBeaconsInRegion方法");
+                String location = "";
+                if (collection.size() > 0) {
+                    //符合要求的beacon集合
+                    List<Beacon> beacons = new ArrayList<>();
+
+                    for (Beacon beacon : collection) {
+                        location = location.concat("UUID: ");
+                        location = location.concat(beacon.getId1().toString());
+                        location = location.concat(", Major: ");
+                        location = location.concat(beacon.getId2().toString());
+                        location = location.concat(", Minor: ");
+                        location = location.concat(beacon.getId3().toString());
+                        location = location.concat("\n");
+                    }
+                }
+                Log.d(TAG, location);
+                updateTextViewMsg(location);
+            }
+
+        });
+        try {
+//            別忘了啓動搜索,不然不會調用didRangeBeaconsInRegion方法
+            beaconManager.startRangingBeaconsInRegion(new Region(FILTER_UUID, null, null, null));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateTextViewMsg(final String location) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                tvLocationMsg.setText(location);
+            }
+        });
+
+    }
+
+    //BLE
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Turn ON BlueTooth if it is OFF
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        }
+
+        setup();
+    }
+
+    private void setup() {
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            pairedDeviceArrayList = new ArrayList<BluetoothDevice>();
+
+            for (BluetoothDevice device : pairedDevices) {
+                pairedDeviceArrayList.add(device);
+            }
+
+            pairedDeviceAdapter = new ArrayAdapter<BluetoothDevice>(this,
+                    android.R.layout.simple_list_item_1, pairedDeviceArrayList);
+            listViewPairedDevice.setAdapter(pairedDeviceAdapter);
+
+            listViewPairedDevice.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    BluetoothDevice device =
+                            (BluetoothDevice) parent.getItemAtPosition(position);
+                    Toast.makeText(MainActivity.this,
+                            "Name: " + device.getName() + "\n"
+                                    + "Address: " + device.getAddress() + "\n"
+                                    + "BondState: " + device.getBondState() + "\n"
+                                    + "BluetoothClass: " + device.getBluetoothClass() + "\n"
+                                    + "Class: " + device.getClass(),
+                            Toast.LENGTH_LONG).show();
+
+                    textStatus.setText("start ThreadConnectBTdevice");
+                    myThreadConnectBTdevice = new ThreadConnectBTdevice(device);
+                    myThreadConnectBTdevice.start();
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //beacon
+        beaconManager.unbind(this);
+        //BLE
+        if(myThreadConnectBTdevice!=null){
+            myThreadConnectBTdevice.cancel();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_OK) {
+                setup();
+            } else {
+                Toast.makeText(this,
+                        "BlueTooth NOT enabled",
+                        Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    //Called in ThreadConnectBTdevice once connect successed
+    //to start ThreadConnected
+    private void startThreadConnected(BluetoothSocket socket){
+
+        myThreadConnected = new ThreadConnected(socket);
+        myThreadConnected.start();
+    }
+
+    /*
+    ThreadConnectBTdevice:
+    Background Thread to handle BlueTooth connecting
+    */
+    private class ThreadConnectBTdevice extends Thread {
+
+        private BluetoothSocket bluetoothSocket = null;
+        private final BluetoothDevice bluetoothDevice;
+
+
+        private ThreadConnectBTdevice(BluetoothDevice device) {
+            bluetoothDevice = device;
+
+            try {
+                bluetoothSocket = device.createRfcommSocketToServiceRecord(myUUID);
+                textStatus.setText("bluetoothSocket: \n" + bluetoothSocket);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            boolean success = false;
+            try {
+                bluetoothSocket.connect();
+                success = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+
+                final String eMessage = e.getMessage();
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        textStatus.setText("something wrong bluetoothSocket.connect(): \n" + eMessage);
+                    }
+                });
+
+                try {
+                    bluetoothSocket.close();
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+            }
+
+            if(success){
+                //connect successful
+                final String msgconnected = "connect successful:\n"
+                        + "BluetoothSocket: " + bluetoothSocket + "\n"
+                        + "BluetoothDevice: " + bluetoothDevice;
+
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        textStatus.setText("");
+//                        textByteCnt.setText("");
+                        Toast.makeText(MainActivity.this, msgconnected, Toast.LENGTH_LONG).show();
+
+                        listViewPairedDevice.setVisibility(View.GONE);
+                        inputPane.setVisibility(View.VISIBLE);
+                    }
+                });
+
+                startThreadConnected(bluetoothSocket);
+
+            }else{
+                //fail
+            }
+        }
+
+        public void cancel() {
+
+            Toast.makeText(getApplicationContext(),
+                    "close bluetoothSocket",
+                    Toast.LENGTH_LONG).show();
+
+            try {
+                bluetoothSocket.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    /*
+    ThreadConnected:
+    Background Thread to handle Bluetooth data communication
+    after connected
+     */
+    private class ThreadConnected extends Thread {
+        private final BluetoothSocket connectedBluetoothSocket;
+        private final InputStream connectedInputStream;
+        private final OutputStream connectedOutputStream;
+
+        public ThreadConnected(BluetoothSocket socket) {
+            connectedBluetoothSocket = socket;
+            InputStream in = null;
+            OutputStream out = null;
+
+            try {
+                in = socket.getInputStream();
+                out = socket.getOutputStream();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            connectedInputStream = in;
+            connectedOutputStream = out;
+        }
+
+        @Override
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            String strRx = "";
+
+            while (true) {
+                try {
+                    bytes = connectedInputStream.read(buffer);  //read!!!!!
+                    final String strReceived = new String(buffer, 0, bytes);
+                    final String strByteCnt = String.valueOf(bytes) + " bytes received.\n";
+
+                    runOnUiThread(new Runnable(){
+
+                        @Override
+                        public void run() {
+                            textStatus.append(strReceived);
+//                            textByteCnt.append(strByteCnt);
+                        }});
+
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+
+                    final String msgConnectionLost = "Connection lost:\n"
+                            + e.getMessage();
+                    runOnUiThread(new Runnable(){
+
+                        @Override
+                        public void run() {
+                            textStatus.setText(msgConnectionLost);
+                        }});
+                }
+            }
+        }
+
+        public void write(byte[] buffer) {
+            try {
+                connectedOutputStream.write(buffer);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        public void cancel() {
+            try {
+                connectedBluetoothSocket.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
